dae83ba33e7acae5039b022fb0b4173c839d9f9e
cmhamel <cmhamel32@gmail.com>
2022-03-06 23:56:38 -0500

    added methods to open existing exodusd databases and read the initialization parameters

--- src/Exodus.jl
+++ src/Exodus.jl
@@ -1,5 +1,79 @@
 module Exodus
 
-greet() = print("Hello World!")
+using Base
+
+const exo_lib_path = ENV["EXODUS_LIB_PATH"]
+
+const EX_WRITE = 0x0001
+const EX_READ = 0x0002
+const EX_NOCLOBBER = 0x0004
+const EX_CLOBBER = 0x0008
+
+const MAX_LINE_LENGTH = 80
+
+const cpu_word_size = Ref{Int64}(sizeof(Float64))
+const IO_word_size = Ref{Int64}(8)
+
+const version_number_1 = 8
+const version_number_2 = 12
+const version_number = 8.12
+
+# types
+#
+ExoFileName = String
+ExoID = Int64
+
+function create_exodus_database(file_name::ExoFileName)
+    """
+    For some reason #define statements are not picked up by ccall
+    """
+    exo_id = ccall((:ex_create_int, exo_lib_path),
+                   Int,
+                   (Base.Cstring, Int64, Ref{Int64}, Ref{Int64}, Int64),
+                   file_name, EX_CLOBBER, cpu_word_size, IO_word_size,
+                   version_number_2)
+    return exo_id
+end
+
+function close_exodus_database(exo_id::ExoID)
+    ccall((:ex_close, exo_lib_path), Int, (Int,), exo_id)
+end
+
+function open_exodus_database(file_name::ExoFileName)
+    exo_id = ccall((:ex_open_int, exo_lib_path),
+                   Int64,
+                   (Base.Cstring, Int64, Ref{Int64},
+                    Ref{Int64}, Ref{Float64}, Int),
+                   file_name, EX_CLOBBER, cpu_word_size, IO_word_size,
+                   version_number, version_number_2)
+    return exo_id
+end
+
+function read_initialization_parameters(exo_id::ExoID)
+    num_dim = Ref{Int64}(0)
+    num_nodes = Ref{Int64}(0)
+    num_elem = Ref{Int64}(0)
+    num_elem_blk = Ref{Int64}(0)
+    num_node_sets = Ref{Int64}(0)
+    num_side_sets = Ref{Int64}(0)
+    error = Ref{Int64}(0)
+
+    title = ""
+    error = ccall((:ex_get_init, exo_lib_path),
+                  Int64,
+                  (ExoID, Base.Cstring,
+                   Ref{Int64}, Ref{Int64}, Ref{Int64},
+                   Ref{Int64}, Ref{Int64}, Ref{Int64}),
+                  exo_id, title,
+                  num_dim, num_nodes, num_elem,
+                  num_elem_blk, num_node_sets, num_side_sets)
+
+    return num_dim[], num_nodes[], num_elem[],
+           num_elem_blk[], num_node_sets[], num_side_sets[]
+end
+
+function read_coordinates(exo_id::ExoID)
+
+end
 
 end # module
---
+++ test/runtests.jl
@@ -0,0 +1,69 @@
+using Exodus
+using Test
+using Profile
+using Suppressor
+
+mesh_file_names = ["../mesh/mesh_test_1.g",
+                   "../mesh/mesh_test_0.5.g",
+                   "../mesh/mesh_test_0.25.g",
+                   "../mesh/mesh_test_0.125.g",
+                   "../mesh/mesh_test_0.0625.g",
+                   "../mesh/mesh_test_0.03125.g",
+                   "../mesh/mesh_test_0.015625.g",
+                   "../mesh/mesh_test_0.0078125.g"]
+
+number_of_nodes = [4, 9, 25, 81, 289, 1089, 4225, 16641]
+number_of_elements = [1, 2^2, 4^2, 8^2, 16^2, 32^2, 64^2, 128^2]
+
+function test_square_mesh(n::Int)
+    mesh_file_name = abspath(mesh_file_names[n])
+    test_name = rpad("Testing square mesh: $(mesh_file_name)", 96)
+    @testset "$test_name" begin
+        @suppress begin
+            # read method test
+            #
+            exo_id = Exodus.open_exodus_database(abspath(mesh_file_names[n]))
+            num_dim, num_nodes, num_elem,
+            num_elem_blk, num_node_sets, num_side_sets =
+            Exodus.read_initialization_parameters(exo_id)
+
+            # tests on the simple numbers
+            #
+            @test num_dim == 2
+            @test num_nodes == number_of_nodes[n]
+            @test num_elem == number_of_elements[n]
+            @test num_elem_blk == 1
+            @test num_node_sets == 4
+            @test num_side_sets == 4
+
+
+            # test coordinates
+            #
+            # coords = Exodus.read_coordinates(exo)
+            # test_coordinate_dimensions(coords, number_of_nodes[n], 2)
+
+            # test block initialization
+            #
+            # block = Mesh.initialize_block(exo, 1)
+            # @test block.block_number == 1
+            # @test block.Nₑ == number_of_elements[n]
+            # @test block.Nₙ_per_e == 4
+            # @test block.element_type == "QUAD4"
+
+            # test node set initialization
+            #
+            # node_set = Mesh.initialize_node_set(exo, 1)
+            # @test node_set.node_set_number == 1
+
+            # test mesh initialization
+            #
+            # mesh = Mesh.MeshStruct()
+
+            Exodus.close_exodus_database(exo_id)
+        end
+    end
+end
+
+for (n, mesh) in enumerate(mesh_file_names)
+    test_square_mesh(n)
+end
