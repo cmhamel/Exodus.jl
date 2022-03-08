8514d3e2edd6e86bdaf52c1da309abdd160fe813
cmhamel <cmhamel32@gmail.com>
2022-03-07 22:47:39 -0500

    added in error checking

--- src/Exodus.jl
+++ src/Exodus.jl
@@ -13,10 +13,12 @@ function create_exodus_database(file_name::ExoFileName)
                    (Base.Cstring, Int64, Ref{Int64}, Ref{Int64}, Int64),
                    file_name, EX_CLOBBER, cpu_word_size, IO_word_size,
                    version_number_2)
+    # TODO: add error checking
     return exo_id
 end
 
 function close_exodus_database(exo_id::ExoID)
+    # TODO: add error checking
     ccall((:ex_close, exo_lib_path), Int, (Int,), exo_id)
 end
 
@@ -27,6 +29,7 @@ function open_exodus_database(file_name::ExoFileName)
                     Ref{Int64}, Ref{Float64}, Int),
                    file_name, EX_CLOBBER, cpu_word_size, IO_word_size,
                    version_number, version_number_2)
+    # TODO: add error checking
     return exo_id
 end
 
@@ -72,20 +75,24 @@ function read_coordinates(exo_id::ExoID, num_dim::Int64, num_nodes::Int64)
         z_coords = Array{Float64}(undef, num_nodes)
     end
 
-    ccall((:ex_get_coord, exo_lib_path), Int64,
-          (ExoID, Ref{Float64}, Ref{Float64}, Ref{Float64}),
-          exo_id, x_coords, y_coords, z_coords)
+    error = ccall((:ex_get_coord, exo_lib_path), Int64,
+                  (ExoID, Ref{Float64}, Ref{Float64}, Ref{Float64}),
+                  exo_id, x_coords, y_coords, z_coords)
+    if error < 0
+        error("Error in read_coordinates\nError = $error")
+    end
     return x_coords, y_coords, z_coords
 end
 
 function read_element_block_parameters(exo_id::ExoID, block_id::BlockID)
-    # element_type = "      "
     element_type = Vector{UInt8}(undef, MAX_STR_LENGTH)
     num_elem = Ref{Int64}(0)
     num_nodes = Ref{Int64}(0)
     num_edges = Ref{Int64}(0)
     num_faces = Ref{Int64}(0)
     num_attributes = Ref{Int64}(0)
+
+    # TODO: add error checking
     ccall((:ex_get_block, exo_lib_path), Int64,
           (ExoID, BlockType, BlockID,
            Ptr{UInt8}, Ref{Int64}, Ref{Int64}, Ref{Int64}, Ref{Int64}, Ref{Int64}),
@@ -117,9 +124,7 @@ end
 function initialize_block(exo_id::ExoID, block_id::BlockID)
     element_type, num_elem, num_nodes, _, _, _ =
     read_element_block_parameters(exo_id::ExoID, block_id::BlockID)
-
     conn = read_block_connectivity(exo_id, block_id)
-
     return Block(block_id, num_elem, num_nodes, element_type, conn)
 end
 
