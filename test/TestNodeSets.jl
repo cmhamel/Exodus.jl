983165a6d6f04f253e5709473b1f8e3ecd2adbab
cmhamel <cmhamel32@gmail.com>
2022-08-07 00:48:54 -0400

    forgot a file

---
+++ test/TestNodeSets.jl
@@ -0,0 +1,27 @@
+mesh_file_names = ["../mesh/square_meshes/mesh_test_1.g",
+                   "../mesh/square_meshes/mesh_test_0.5.g",
+                   "../mesh/square_meshes/mesh_test_0.25.g",
+                   "../mesh/square_meshes/mesh_test_0.125.g",
+                   "../mesh/square_meshes/mesh_test_0.0625.g",
+                   "../mesh/square_meshes/mesh_test_0.03125.g",
+                   "../mesh/square_meshes/mesh_test_0.015625.g",
+                   "../mesh/square_meshes/mesh_test_0.0078125.g"]
+
+number_of_nodes = [4, 9, 25, 81, 289, 1089, 4225, 16641]
+number_of_elements = [1, 2^2, 4^2, 8^2, 16^2, 32^2, 64^2, 128^2]
+number_of_node_set_nodes = [1, 2, 4, 8, 16, 32, 64, 128] .+ 1
+
+function test_read_node_set_ids_on_square_meshes(n::Int64)
+    exo = Exodus.open_exodus_database(abspath(mesh_file_names[n]))
+    init = Exodus.Initialization(exo)
+    nset_ids = Exodus.read_node_set_ids(exo, init.num_node_sets)
+    @test length(nset_ids) == 4
+    @test nset_ids == [1, 2, 3, 4]
+    Exodus.close_exodus_database(exo)
+end
+
+@exodus_unit_test_set "Square Mesh Test Read Node Set IDs" begin
+    for (n, mesh) in enumerate(mesh_file_names)
+        test_read_node_set_ids_on_square_meshes(n)
+    end
+end
