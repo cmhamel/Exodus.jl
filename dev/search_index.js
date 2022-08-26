b781b65e159839fce77b3d3ba5beb34036564779
Documenter.jl <documenter@juliadocs.github.io>
2022-08-26 18:48:24 +0000

    build based on 2af7223

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Exodus.jl","page":"Exodus.jl","title":"Exodus.jl","text":"","category":"section"},{"location":"","page":"Exodus.jl","title":"Exodus.jl","text":"Documentation for Exodus.jl","category":"page"},{"location":"#Documentation","page":"Exodus.jl","title":"Documentation","text":"","category":"section"},{"location":"","page":"Exodus.jl","title":"Exodus.jl","text":"Modules = [Exodus]","category":"page"}]
+}
