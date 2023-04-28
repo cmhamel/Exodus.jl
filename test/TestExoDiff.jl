03804f048d463cdeee85b42cf971f1d989399a7b
cmhamel <cmhamel32@gmail.com>
2023-04-28 13:05:03 -0400

    re-enabling windows and making exodiff skip that test on windows.

--- test/TestExoDiff.jl
+++ test/TestExoDiff.jl
@@ -1,3 +1,7 @@
 @exodus_unit_test_set "exodiff" begin
-    @exodiff "./example_output/output.gold" "./example_output/output.gold"
+    if Sys.iswindows()
+        @show "skipping exodiff tests for windows"
+    else
+        @exodiff "./example_output/output.gold" "./example_output/output.gold"
+    end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Test
 macro exodus_unit_test_set(test_name::String, ex)
     return quote
         local test_set_name = rpad($test_name, 64)
-        @testset "$test_set_name" begin
+        @testset verbose = true "$test_set_name" begin
             local val = $ex
             val
         end
