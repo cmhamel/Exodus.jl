43dce45b404130c13aefb0ce278a3d624d1b4458
cmhamel <cmhamel32@gmail.com>
2023-06-06 08:32:19 -0400

    add test set extensions to exodus_unit_test macro

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using TestSetExtensions
 macro exodus_unit_test_set(test_name::String, ex)
   return quote
     local test_set_name = rpad($test_name, 64)
-    @testset verbose = true "$test_set_name" begin
+    @testset ExtendedTestSet "$test_set_name" begin
       local val = $ex
       val
     end
