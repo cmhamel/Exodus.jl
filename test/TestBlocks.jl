58f8f5ba1ada16c1308ddb9be77dede286e506ae
cmhamel <cmhamel32@gmail.com>
2023-06-09 09:58:38 -0400

    added 2D/3D tests to blocks and coordinates.

--- test/TestBlocks.jl
+++ test/TestBlocks.jl
@@ -1,10 +1,14 @@
-mesh_file_name = "./mesh/square_meshes/mesh_test_0.0078125.g"
-number_of_nodes = 16641
-number_of_elements = 128^2
+mesh_file_name_2D = "./mesh/square_meshes/mesh_test_0.0078125.g"
+number_of_nodes_2D = 16641
+number_of_elements_2D = 128^2
 
+
+mesh_file_name_3D = "./mesh/cube_meshes/mesh_test_0.125.g"
+number_of_nodes_3D = 729
+number_of_elements_3D = 512
 
 function test_read_block_ids_on_square_meshes()
-  exo = ExodusDatabase(abspath(mesh_file_name), "r")
+  exo = ExodusDatabase(abspath(mesh_file_name_2D), "r")
   block_ids = read_block_ids(exo)
   @test length(block_ids) == 1
   @test block_ids == [1]
@@ -12,27 +16,62 @@ function test_read_block_ids_on_square_meshes()
 end
 
 function test_read_blocks_on_square_meshes()
-  exo = ExodusDatabase(abspath(mesh_file_name), "r")
+  exo = ExodusDatabase(abspath(mesh_file_name_2D), "r")
   block_ids = read_block_ids(exo)
   blocks = read_blocks(exo, block_ids)
   @test length(blocks)               == 1
   @test blocks[1].block_id           == 1
-  @test blocks[1].num_elem           == number_of_elements
+  @test blocks[1].num_elem           == number_of_elements_2D
   @test blocks[1].num_nodes_per_elem == 4
   @test blocks[1].elem_type          == "QUAD4"
-  # @test length(blocks[1].conn)       == 4 * number_of_elements
-  # @test size(blocks[1].conn)         == (number_of_elements, 4)
-  @test size(blocks[1].conn)         == (4, number_of_elements)
+  @test size(blocks[1].conn)         == (4, number_of_elements_2D)
+  close(exo)
+end
+
+function test_read_block_ids_on_cube_meshes()
+  exo = ExodusDatabase(abspath(mesh_file_name_3D), "r")
+  block_ids = read_block_ids(exo)
+  @test length(block_ids) == 1
+  @test block_ids == [1]
+  close(exo)
+end
+
+function test_read_blocks_on_cube_meshes()
+  exo = ExodusDatabase(abspath(mesh_file_name_3D), "r")
+  block_ids = read_block_ids(exo)
+  blocks = read_blocks(exo, block_ids)
+  @test length(blocks)               == 1
+  @test blocks[1].block_id           == 1
+  @test blocks[1].num_elem           == number_of_elements_3D
+  @test blocks[1].num_nodes_per_elem == 8
+  @test blocks[1].elem_type          == "HEX8"
+  @test size(blocks[1].conn)         == (8, number_of_elements_3D)
   close(exo)
 end
 
 @exodus_unit_test_set "Blocks.jl - Read" begin
   test_read_block_ids_on_square_meshes()
   test_read_blocks_on_square_meshes()
+  test_read_block_ids_on_cube_meshes()
+  test_read_blocks_on_cube_meshes()
 end
 
-@exodus_unit_test_set "Test block read from name" begin
-  exo = ExodusDatabase(abspath(mesh_file_name), "r")
+@exodus_unit_test_set "Test block read from name 2D" begin
+  exo = ExodusDatabase(abspath(mesh_file_name_2D), "r")
+  block_1 = Block(exo, "block_1")
+  block_2 = Block(exo, 1)
+
+  @test block_1.block_id           == block_2.block_id
+  @test block_1.num_elem           == block_2.num_elem
+  @test block_1.num_nodes_per_elem == block_2.num_nodes_per_elem
+  @test block_1.elem_type          == block_2.elem_type
+  @test block_1.conn               == block_2.conn
+
+  close(exo)
+end
+
+@exodus_unit_test_set "Test block read from name 3D" begin
+  exo = ExodusDatabase(abspath(mesh_file_name_3D), "r")
   block_1 = Block(exo, "block_1")
   block_2 = Block(exo, 1)
 
--- test/TestCoordinates.jl
+++ test/TestCoordinates.jl
@@ -1,29 +1,48 @@
-mesh_file_name = "./mesh/square_meshes/mesh_test_0.0078125.g"
-number_of_nodes = 16641
-number_of_elements = 128^2
+mesh_file_name_2D = "./mesh/square_meshes/mesh_test_0.0078125.g"
+number_of_nodes_2D = 16641
+number_of_elements_2D = 128^2
+
+mesh_file_name_3D = "./mesh/cube_meshes/mesh_test_0.125.g"
+number_of_nodes_3D = 729
+number_of_elements_3D = 512
 
 function test_read_coordinates_on_square_mesh()
-  exo = ExodusDatabase(abspath(mesh_file_name), "r")
+  exo = ExodusDatabase(abspath(mesh_file_name_2D), "r")
   coords = read_coordinates(exo)
-  @test size(coords) == (2, number_of_nodes)
-  # @test size(coords) == (number_of_nodes, 2)
+  @test size(coords) == (2, number_of_nodes_2D)
   close(exo)
 end
 
 function test_read_coordinate_names_on_square_mesh()
-  exo = ExodusDatabase(abspath(mesh_file_name), "r")
+  exo = ExodusDatabase(abspath(mesh_file_name_2D), "r")
   coord_names = read_coordinate_names(exo)
   @test coord_names == ["x", "y"]
+  close(exo)
+end
+
+function test_read_coordinates_on_cube_mesh()
+  exo = ExodusDatabase(abspath(mesh_file_name_3D), "r")
+  coords = read_coordinates(exo)
+  @test size(coords) == (3, number_of_nodes_3D)
+  close(exo)
+end
+
+function test_read_coordinate_names_on_cube_mesh()
+  exo = ExodusDatabase(abspath(mesh_file_name_3D), "r")
+  coord_names = read_coordinate_names(exo)
+  @test coord_names == ["x", "y", "z"]
   close(exo)
 end
 
 @exodus_unit_test_set "Coorinates.jl - Read" begin
   test_read_coordinates_on_square_mesh()
   test_read_coordinate_names_on_square_mesh()
+  test_read_coordinates_on_cube_mesh()
+  test_read_coordinate_names_on_cube_mesh()
 end
 
 function test_write_coordinates_on_square_mesh()
-  exo_old = ExodusDatabase(abspath(mesh_file_name), "r")
+  exo_old = ExodusDatabase(abspath(mesh_file_name_2D), "r")
   init_old = Initialization(exo_old)
   coords_old = read_coordinates(exo_old)
   close(exo_old)
@@ -40,7 +59,7 @@ function test_write_coordinates_on_square_mesh()
 end
 
 function test_write_coordinate_names_on_square_mesh()
-  exo_old = ExodusDatabase(abspath(mesh_file_name), "r")
+  exo_old = ExodusDatabase(abspath(mesh_file_name_2D), "r")
   init_old = exo_old.init
   coord_names_old = read_coordinate_names(exo_old)
   close(exo_old)
@@ -57,7 +76,45 @@ function test_write_coordinate_names_on_square_mesh()
   Base.Filesystem.rm("./test_output_coordinate_names.e")
 end
 
+function test_write_coordinates_on_cube_mesh()
+  exo_old = ExodusDatabase(abspath(mesh_file_name_3D), "r")
+  init_old = Initialization(exo_old)
+  coords_old = read_coordinates(exo_old)
+  close(exo_old)
+
+  exo_new = ExodusDatabase("./test_output_coords.e", init_old)
+  write_coordinates(exo_new, coords_old)
+  coords_new = read_coordinates(exo_new)
+  close(exo_new)
+
+  @test init_old == exo_new.init
+  @test coords_old == coords_new
+
+  Base.Filesystem.rm("./test_output_coords.e")
+end
+
+function test_write_coordinate_names_on_cube_mesh()
+  exo_old = ExodusDatabase(abspath(mesh_file_name_3D), "r")
+  init_old = exo_old.init
+  coord_names_old = read_coordinate_names(exo_old)
+  close(exo_old)
+
+  exo_new = ExodusDatabase("./test_output_coordinate_names.e", init_old)
+  write_coordinate_names(exo_new, coord_names_old)
+  coord_names_new = read_coordinate_names(exo_new)
+  close(exo_new)
+
+  @test coord_names_new == coord_names_old
+  @test coord_names_new[1] == "x"
+  @test coord_names_new[2] == "y"
+  @test coord_names_new[3] == "z"
+
+  Base.Filesystem.rm("./test_output_coordinate_names.e")
+end
+
 @exodus_unit_test_set "Coordinates.jl - Write" begin
   test_write_coordinates_on_square_mesh()
   test_write_coordinate_names_on_square_mesh()
+  test_write_coordinates_on_cube_mesh()
+  test_write_coordinate_names_on_cube_mesh()
 end
