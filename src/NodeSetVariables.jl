9506b5b5ac0625607cc3c0a56ae1a97bdf59a8b1
Craig M. Hamel <cmhamel32@gmail.com>
2023-07-01 02:52:55 -0400

    removing ex methods for NodeSetVariables.jl

--- src/NodeSetVariables.jl
+++ src/NodeSetVariables.jl
@@ -75,7 +75,6 @@ end
 """
 """
 function write_number_of_node_set_variables(exo::ExodusDatabase, num_vars)
-  # ex_put_variable_param!(get_file_id(exo), EX_NODE_SET, num_vars)
   error_code = @ccall libexodus.ex_put_variable_param(
     get_file_id(exo)::Cint, EX_NODE_SET::ex_entity_type, num_vars::Cint
   )::Cint
@@ -86,7 +85,6 @@ end
 """
 function write_node_set_variable_name(exo::ExodusDatabase, var_index::Integer, var_name::String)
   temp = Vector{UInt8}(var_name)
-  # ex_put_variable_name!(get_file_id(exo), EX_NODE_SET, var_index, temp)
   error_code = @ccall libexodus.ex_put_variable_name(
     get_file_id(exo)::Cint, EX_NODE_SET::ex_entity_type, var_index::Cint, temp::Ptr{UInt8}
   )::Cint
@@ -102,7 +100,6 @@ function write_node_set_variable_names(exo::ExodusDatabase, var_indices::Vector{
 
   for n in axes(var_indices, 1)
     temp = Vector{UInt8}(var_names[n])
-    # ex_put_variable_name!(get_file_id(exo), EX_NODE_SET, convert(Cint, var_indices[n]), temp)
     error_code = @ccall libexodus.ex_put_variable_name(
       get_file_id(exo)::Cint, EX_NODE_SET::ex_entity_type, var_indices[n]::Cint, temp::Ptr{UInt8}
     )::Cint
@@ -112,14 +109,16 @@ end
 
 """
 """
-function write_node_set_variable_values(exo::ExodusDatabase, time_step, 
+function write_node_set_variable_values(exo::ExodusDatabase, timestep::Integer, 
                                         var_index::Integer, nset_id, var_values::Vector{<:Real}) # TODO add types
   var_index = convert(get_id_int_type(exo), var_index)
   num_nodes = size(var_values, 1)
-  # TODO probably need some size error checking
-  ex_put_var!(get_file_id(exo), convert(Cint, time_step), EX_NODE_SET, 
-              convert(Cint, var_index), nset_id, 
-              convert(Clonglong, num_nodes), var_values)
+  error_code = @ccall libexodus.ex_put_var(
+    get_file_id(exo)::Cint, timestep::Cint, EX_NODE_SET::ex_entity_type,
+    var_index::Cint, nset_id::ex_entity_id,
+    num_nodes::Clonglong, var_values::Ptr{Cvoid}
+  )::Cint
+  exodus_error_check(error_code, "Exodus.write_node_set_variable_values -> libexodus.ex_put_var")
 end
 
 """
