ab381bf1951655e69d4275fe007c0aa41a3a4070
Craig M. Hamel <cmhamel32@gmail.com>
2023-07-02 10:57:58 -0400

    adding write qa method and accompanying test.

--- src/QA.jl
+++ src/QA.jl
@@ -2,13 +2,6 @@ function read_qa(exo::ExodusDatabase)
   num_qa_rec = @ccall libexodus.ex_inquire_int(
     get_file_id(exo)::Cint, EX_INQ_QA::ex_inquiry
   )::Cint
-  # qa_record = Vector{Vector{Vector{UInt8}}}(undef, num_qa_rec)
-  # for i in 1:num_qa_rec
-  #   qa_record[i] = Vector{Vector{UInt8}}(undef, 4)
-  #   for j in 1:4
-  #     qa_record[i][j] = Vector{UInt8}(undef, MAX_STR_LENGTH)
-  #   end
-  # end
   qa_record = Matrix{Vector{UInt8}}(undef, num_qa_rec, 4)
   for i in 1:num_qa_rec
     for j in 1:4
@@ -29,4 +22,13 @@ function read_qa(exo::ExodusDatabase)
   return new_qa_record
 end
 
+function write_qa(exo::ExodusDatabase, qa_record::Matrix{String})
+  num_qa_records = size(qa_record, 1)
+  error_code = @ccall libexodus.ex_put_qa(
+    get_file_id(exo)::Cint, num_qa_records::Cint, qa_record::Ptr{Ptr{UInt8}}
+  )::Cint
+  exodus_error_check(error_code, "Exodus.write_qa -> libexodus.ex_put_qa")
+end
+
 export read_qa
+export write_qa
--- test/TestQA.jl
+++ test/TestQA.jl
@@ -8,3 +8,23 @@
   @test qa[1, 3] == "06/29/2023" # may change
   @test qa[1, 4] == "19:34:08"   # may change
 end
+
+@exodus_unit_test_set "Test read/write/read QA" begin
+  exo = ExodusDatabase("./mesh/square_meshes/mesh_test.g", "r")
+  init = exo.init
+  qa_old = read_qa(exo)
+  close(exo)
+
+  exo = ExodusDatabase("./qa_temp.e", init)
+  write_qa(exo, qa_old)
+  close(exo)
+ 
+  exo = ExodusDatabase("./qa_temp.e", "r")
+  qa_new = read_qa(exo)
+  close(exo)
+  for n in eachindex(qa_old)
+    @test qa_old[n] == qa_new[n]
+  end
+
+  rm("./qa_temp.e")
+end
