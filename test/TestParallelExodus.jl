4c852abf2ea77056fcde3b1ef1f15dbe555874b2
Craig M. Hamel <cmhamel32@gmail.com>
2023-07-11 22:48:21 -0400

    skipping parallel exodus testing on windows.

--- test/TestParallelExodus.jl
+++ test/TestParallelExodus.jl
@@ -1,7 +1,7 @@
-@testset ExtendedTestSet "ParallelExodusDatabase" begin
-  if sys.iswindows()
-    # weirdly erroring out here
-  else
+if sys.iswindows()
+  # weirdly not working here
+else
+  @testset ExtendedTestSet "ParallelExodusDatabase" begin
     exo = ParallelExodusDatabase("mesh/square_meshes/mesh_test.g", 4)
     close(exo)
   end
