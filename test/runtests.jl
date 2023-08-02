c51c5791164bc8bc4f99becf7da226343bc74e41
Craig M. Hamel <cmhamel32@gmail.com>
2023-08-02 00:24:52 -0400

    moving/deleting around other test files.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,9 @@
-import Base.Filesystem: copy
 using Aqua
 using Exodus
 using Test
 using TestSetExtensions
 
+# macro for testing
 macro exodus_unit_test_set(test_name::String, ex)
   return quote
     local test_set_name = rpad($test_name, 64)
@@ -14,16 +14,61 @@ macro exodus_unit_test_set(test_name::String, ex)
   end
 end
 
+# decomp tests
+@exodus_unit_test_set "decomp - 2d" begin
+  @decomp "./mesh/square_meshes/mesh_test.g" 16
+  for n in 0:15
+    rm("./mesh/square_meshes/mesh_test.g.16." * lpad(n, 2, "0"), force=true)
+  end
+  rm("./mesh/square_meshes/mesh_test.g.nem", force=true)
+  rm("./mesh/square_meshes/mesh_test.g.pex", force=true)
+  rm("./mesh/square_meshes/decomp.log", force=true)
+end
+
+@exodus_unit_test_set "decomp - 3d" begin
+  @decomp "./mesh/cube_meshes/mesh_test.g" 16
+  for n in 0:15
+    rm("./mesh/cube_meshes/mesh_test.g.16." * lpad(n, 2, "0"), force=true)
+  end
+  rm("./mesh/cube_meshes/mesh_test.g.nem", force=true)
+  rm("./mesh/cube_meshes/mesh_test.g.pex", force=true)
+  rm("./mesh/cube_meshes/decomp.log", force=true)
+end
+
+@exodus_unit_test_set "decomp methods" begin
+  decomp("./mesh/square_meshes/mesh_test.g", 16)
+  for n in 0:15
+    rm("./mesh/square_meshes/mesh_test.g.16." * lpad(n, 2, "0"), force=true)
+  end
+  rm("./mesh/square_meshes/mesh_test.g.nem", force=true)
+  rm("./mesh/square_meshes/mesh_test.g.pex", force=true)
+  rm("./mesh/square_meshes/decomp.log", force=true)
+end
+
+# epu test
+@exodus_unit_test_set "EPU test" begin
+  if !Sys.iswindows()
+    @epu "./mesh/square_meshes/epu_mesh_test.g"
+    @exodiff "epu_mesh_test.g" "./mesh/square_meshes/mesh_test.g"
+  end
+end 
+
 # simple test of error handling capability
 @exodus_unit_test_set "Test Errors working" begin
   @test_throws ErrorException Exodus.exodus_error_check(-1, "JohnSmithMethod")
 end
 
+# exodiff tests
+@exodus_unit_test_set "exodiff" begin
+  if Sys.iswindows()
+    @show "skipping exodiff tests for windows"
+  else
+    @exodiff "./example_output/output.gold" "./example_output/output.gold"
+    rm("./exodiff.log", force=true)
+  end
+end
+
 # @includetests ARGS
-include("TestDecomp.jl")
-# include("TestEpu.jl")
-include("TestErrors.jl")
-include("TestExoDiff.jl")
 include("TestRead.jl")
 include("TestReadWrite.jl")
 include("TestWrite.jl")
