7e90397f95e17cf076d8fa717f3bbd8fcd71cffa
Craig M. Hamel <cmhamel32@gmail.com>
2023-08-12 14:34:43 -0400

    adding macro to all methods with ccalls in sets to start.

--- src/Errors.jl
+++ src/Errors.jl
@@ -1,12 +1,23 @@
 struct ExodusError <: Exception
   error_code::Cint
-  # exodus_jl_method_name::String
-  # exodus_ii_method_name::String
   error_message::String
 end
 
+struct ExodusError2 <: Exception
+  error_code::Cint
+  exodus_jl_method_name::String
+  exodus_ii_method_name::String
+end
+
+function Base.show(io::IO, e::ExodusError2)
+  println(io, "")
+  println(io, "ExodusII library error thrown with error code $(e.error_code).")
+  print(io, "Error caused by Exodus.jl wrapper method \"$(e.exodus_jl_method_name)\" ")
+  println(io, "which calls the exodusII library method \"$(e.exodus_ii_method_name)\"")
+end
+
 function Base.show(io::IO, e::ExodusError)
-  println(io, "ExodusII error thrown with error code $(e.error_code).")
+  println(io, "ExodusII library error thrown with error code $(e.error_code).")
   println(io, "Error message: $(e.error_message)")
 end
 
@@ -18,13 +29,38 @@ Generic error handling method.
 """
 function exodus_error_check(error_code::T, error_message::String) where T <: Integer
   if error_code < 0
-    # error("Error from exodus library call in method $method_name with code $error_code")
     throw(ExodusError(error_code, error_message))
   end
 end
 
+function exodus_ii_error_check(
+  error_code::T, 
+  exodus_jl_method_name::String,
+  exodus_ii_method_name::String
+) where T <: Integer
 
-# id_error(exo, ::Type{t}, id) where t <: AbstractSet = throw(SetIDException(exo, t, id))
-# name_error(exo, ::Type{t}, name) where t <: AbstractSet = throw(SetNameException(exo, t, name))
-# id_error(exo, ::Type{t}, id) where t <: AbstractVariable = throw(VariableIDException(exo, t, id))
-# name_error(exo, ::Type{t}, name) where t <: AbstractVariable = throw(VariableNameException(exo, t, name))
+  if error_code < 0
+    throw(ExodusError2(error_code, exodus_jl_method_name, exodus_ii_method_name))
+  end
+end
+
+macro exodus_ii_error_check(ex)
+  local exodus_jl_method_name = split(string(ex.args[1].args[1]), "(")[1] |> String
+
+  for (n, expr) in enumerate(ex.args[2].args)
+    if !(hasproperty(expr, :args)) 
+      continue
+    end
+
+    for arg in expr.args
+      local str_arg = string(arg)
+      if occursin("libexodus", str_arg)
+        local exodus_ii_method_name = split(split(str_arg, "libexodus.")[2], "(")[1] |> String
+        local error_expr = Expr(:call, :exodus_ii_error_check, 
+                                :error_code, Expr(:quote, exodus_jl_method_name), Expr(:quote, exodus_ii_method_name))
+        insert!(ex.args[2].args, n + 1, error_expr)
+        return ex
+      end
+    end
+  end
+end
--- src/Sets.jl
+++ src/Sets.jl
@@ -1,4 +1,4 @@
-function read_ids(exo::ExodusDatabase{M, I, B, F}, ::Type{S}) where {M, I, B, F, S <: AbstractSet}
+@exodus_ii_error_check function read_ids(exo::ExodusDatabase{M, I, B, F}, ::Type{S}) where {M, I, B, F, S <: AbstractSet}
   if S <: Block
     num_entries = exo.init.num_elem_blks
   elseif S <: NodeSet
@@ -17,13 +17,12 @@ function read_ids(exo::ExodusDatabase{M, I, B, F}, ::Type{S}) where {M, I, B, F,
   error_code = @ccall libexodus.ex_get_ids(
     get_file_id(exo)::Cint, entity_type(S)::ex_entity_type, ids::Ptr{B}
   )::Cint
-  exodus_error_check(error_code, "Exodus.read_set_ids -> libexodus.ex_get_ids")
   return ids
 end
 
 """
 """
-function read_name(exo::ExodusDatabase, ::Type{S}, id::Integer) where S <: AbstractSet
+@exodus_ii_error_check function read_name(exo::ExodusDatabase, ::Type{S}, id::Integer) where S <: AbstractSet
   name = exo.cache_uint8
   resize!(name, MAX_STR_LENGTH)
 
@@ -31,7 +30,7 @@ function read_name(exo::ExodusDatabase, ::Type{S}, id::Integer) where S <: Abstr
     get_file_id(exo)::Cint, entity_type(S)::ex_entity_type, 
     id::ex_entity_id, name::Ptr{UInt8}
   )::Cint
-  exodus_error_check(error_code, "Exodus.read_name -> libexodus.ex_get_name")
+
   return unsafe_string(pointer(name))
 end
 
@@ -67,20 +66,20 @@ end
 
 """
 """
-function read_set_parameters(exo::ExodusDatabase{M, I, B, F}, set_id::Integer, ::Type{S}) where {M, I, B, F, S <: AbstractSet}
+@exodus_ii_error_check function read_set_parameters(exo::ExodusDatabase{M, I, B, F}, set_id::Integer, ::Type{S}) where {M, I, B, F, S <: AbstractSet}
   num_entries = Ref{I}(0)
   num_df = Ref{I}(0)
+
   error_code = @ccall libexodus.ex_get_set_param(
     get_file_id(exo)::Cint, entity_type(S)::ex_entity_type, set_id::Clonglong, # set_id is really an ex_entity_id but that is weirdly causing a type instability
     num_entries::Ptr{I}, num_df::Ptr{I}
   )::Cint
-  exodus_error_check(error_code, "Exodus.read_set_parameters -> libexodus.ex_get_set_param")
   return num_entries[], num_df[]
 end
 
 """
 """
-function read_node_set_nodes(exo::ExodusDatabase{M, I, B, F}, set_id::Integer) where {M, I, B, F}
+@exodus_ii_error_check function read_node_set_nodes(exo::ExodusDatabase{M, I, B, F}, set_id::Integer) where {M, I, B, F}
   num_entries, _ = read_set_parameters(exo, set_id, NodeSet)
   entries = exo.cache_B_1
   resize!(entries, num_entries)
@@ -90,17 +89,17 @@ function read_node_set_nodes(exo::ExodusDatabase{M, I, B, F}, set_id::Integer) w
   end
 
   extras = C_NULL
-    error_code = @ccall libexodus.ex_get_set(
+
+  error_code = @ccall libexodus.ex_get_set(
     get_file_id(exo)::Cint, EX_NODE_SET::ex_entity_type, set_id::Clonglong, # set id is really a ex_entity_id but it's weirldy throwing a type instability
     entries::Ptr{B}, extras::Ptr{Cvoid}
   )::Cint
-  exodus_error_check(error_code, "Exodus.read_node_set_nodes -> libexodus.ex_get_set")
   return entries
 end
 
 """
 """
-function read_side_set_elements_and_sides(exo::ExodusDatabase{M, I, B, F}, set_id::Integer) where {M, I, B, F}
+@exodus_ii_error_check function read_side_set_elements_and_sides(exo::ExodusDatabase{M, I, B, F}, set_id::Integer) where {M, I, B, F}
   num_entries, _ = read_set_parameters(exo, set_id, SideSet)
 
   entries = exo.cache_B_1
@@ -118,12 +117,13 @@ function read_side_set_elements_and_sides(exo::ExodusDatabase{M, I, B, F}, set_i
     get_file_id(exo)::Cint, EX_SIDE_SET::ex_entity_type, set_id::Clonglong, # set id is really a ex_entity_id but it's weirldy throwing a type instability
     entries::Ptr{B}, extras::Ptr{B}
   )::Cint
-  exodus_error_check(error_code, "Exodus.read_side_set_elements_and_sides -> libexodus.ex_get_set")
+
   return entries, extras
 end
 
 """
-UNTESTED
+UNTESTED... also edge case for current exodus_ii_error_check method
+due to multiple ccalls here
 """
 function read_side_set_node_list(exo::ExodusDatabase{M, I, B, F}, side_set_id::Integer) where {M, I, B, F}
   side_set_node_list_len = Ref{Cint}(0)
@@ -194,26 +194,24 @@ end
 WARNING:
 currently doesn't support distance factors
 """
-function write_set_parameters(exo::ExodusDatabase{M, I, B, F}, set::T) where {M, I, B, F, T <: AbstractSet}
+@exodus_ii_error_check function write_set_parameters(exo::ExodusDatabase{M, I, B, F}, set::T) where {M, I, B, F, T <: AbstractSet}
   num_dist_fact_in_set = 0 # TODO not using distance 
   error_code = @ccall libexodus.ex_put_set_param(
     get_file_id(exo)::Cint, entity_type(T)::ex_entity_type, set.id::Clonglong, # should be ex_entity_id
     length(set)::Clonglong, num_dist_fact_in_set::Clonglong
   )::Cint
-  exodus_error_check(error_code, "Exodus.write_node_set_parameters -> libexodus.ex_put_set_param")
 end
 
 """
 Typing ensures we don't write a set with non-matching types
 to the exodus file.
 """
-function write_set(exo::ExodusDatabase{M, I, B, F}, set::T) where {T <: AbstractSet, M, I, B, F}
+@exodus_ii_error_check function write_set(exo::ExodusDatabase{M, I, B, F}, set::T) where {T <: AbstractSet, M, I, B, F}
   write_set_parameters(exo, set)
   error_code = @ccall libexodus.ex_put_set(
     get_file_id(exo)::Cint, entity_type(T)::ex_entity_type, set.id::Clonglong, # should be ex_entity_id
     entries(set)::Ptr{B}, extras(set)::Ptr{Union{Cvoid, B}}
   )::Cint
-  exodus_error_check(error_code, "Exodus.write_set -> libexodus.ex_put_set")
 end
 
 """
@@ -226,7 +224,7 @@ end
 
 """
 """
-function write_name(exo::ExodusDatabase{M, I, B, F}, ::Type{S}, set_id::Integer, name::String) where {M, I, B, F, S <: AbstractSet}
+@exodus_ii_error_check function write_name(exo::ExodusDatabase{M, I, B, F}, ::Type{S}, set_id::Integer, name::String) where {M, I, B, F, S <: AbstractSet}
   
   if S <: Block
     exo.block_name_dict[name] = set_id
@@ -240,12 +238,11 @@ function write_name(exo::ExodusDatabase{M, I, B, F}, ::Type{S}, set_id::Integer,
     get_file_id(exo)::Cint, entity_type(S)::ex_entity_type, set_id::Clonglong, # should really be ex_entity_id
     name::Ptr{UInt8}
   )::Cint
-  exodus_error_check(error_code, "Exodus.write_set_name -> libexodus.ex_put_name")
 end
 
 """
 """
-function write_name(exo::ExodusDatabase{M, I, B, F}, set::S, name::String) where {M, I, B, F, S <: AbstractSet}
+@exodus_ii_error_check function write_name(exo::ExodusDatabase{M, I, B, F}, set::S, name::String) where {M, I, B, F, S <: AbstractSet}
   if S <: Block
     exo.block_name_dict[name] = set.id
   elseif S <: NodeSet
@@ -258,15 +255,13 @@ function write_name(exo::ExodusDatabase{M, I, B, F}, set::S, name::String) where
     get_file_id(exo)::Cint, entity_type(S)::ex_entity_type, set.id::Clonglong, # should really be ex_entity_id
     name::Ptr{UInt8}
   )::Cint
-  exodus_error_check(error_code, "Exodus.write_set_name -> libexodus.ex_put_name")
 end
 
 """
 WARNING: this methods likely does not have good safe guards
 """
-function write_names(exo::ExodusDatabase, ::Type{S}, names::Vector{String}) where S <: AbstractSet
+@exodus_ii_error_check function write_names(exo::ExodusDatabase, ::Type{S}, names::Vector{String}) where S <: AbstractSet
   error_code = @ccall libexodus.ex_put_names(
     get_file_id(exo)::Cint, entity_type(S)::ex_entity_type, names::Ptr{Ptr{UInt8}}
   )::Cint
-  exodus_error_check(error_code, "Exodus.write_set_names -> libexodus.ex_put_names")
 end
