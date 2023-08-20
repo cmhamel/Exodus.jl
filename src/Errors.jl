6bcd505ccd8cb5ccc261ef0239a85b7a6d481c1b
Craig M. Hamel <cmhamel32@gmail.com>
2023-08-19 23:09:49 -0400

    adding a proper Error exception so we stop abusing error for everything.

--- src/Errors.jl
+++ src/Errors.jl
@@ -1,4 +1,16 @@
 """
+"""
+struct ExodusError <: Exception
+  error_code::Cint
+  error_msg::String
+end
+
+"""
+"""
+Base.show(io::IO, e::ExodusError) = 
+println(io, "Error from exodusII library in method $(e.method_name) with code $(e.error_code)")
+
+"""
 Generic error handling method.
 # Arguments
 - `error_code::T`: error code, usually negative means something went bad
@@ -6,12 +18,6 @@ Generic error handling method.
 """
 function exodus_error_check(error_code::T, method_name::String) where {T <: Integer}
   if error_code < 0
-    error("Error from exodus library call in method $method_name with code $error_code")
+    throw(ExodusError(error_code, method_name))
   end
 end
-
-
-# id_error(exo, ::Type{t}, id) where t <: AbstractSet = throw(SetIDException(exo, t, id))
-# name_error(exo, ::Type{t}, name) where t <: AbstractSet = throw(SetNameException(exo, t, name))
-# id_error(exo, ::Type{t}, id) where t <: AbstractVariable = throw(VariableIDException(exo, t, id))
-# name_error(exo, ::Type{t}, name) where t <: AbstractVariable = throw(VariableNameException(exo, t, name))
